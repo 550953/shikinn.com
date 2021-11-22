b21fab2c97dcaab13a440cf729ca3f9f5a8a6b5b
shikinn <550953@gmail.com>
2021-11-22 22:30:56 +0300

    First Commit
    Файлы с домена shikinn.com

---
+++ shikinn.com/index.php
@@ -0,0 +1,22 @@
+<html><head><script async="" src="https://mc.yandex.ru/metrika/tag.js"></script>
+</head>
+<link rel="icon" type="image/png" sizes="16x16"  href="/favicons/favicon.png">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+
+<body>
+<img src="shikinn.png" alt="Shikinn.com"><br>+7 978 124 84 90  Shikin N.N webmaster<br>+7 (495) 138-26-48
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(82438051, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/82438051" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter --></body></html>
